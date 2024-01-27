9acc615eeda11b880d32dc66d51c4be4c76055b3
brrrrrrrrrr <brrrrrrrrrr@brrrrrrrrrr>
2024-01-27 16:41:38 -0500

    Add picodrive

---
+++ package/libretro-picodrive/libretro-picodrive.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# libretro-picodrive
+#
+################################################################################
+
+LIBRETRO_PICODRIVE_VERSION = d907d65692a45e126d0c7d6685cc8792b52bc577
+LIBRETRO_PICODRIVE_SITE = $(call github,libretro,picodrive,$(LIBRETRO_PICODRIVE_VERSION))
+LIBRETRO_PICODRIVE_LICENSE = COPYRIGHT
+LIBRETRO_PICODRIVE_LICENSE_FILES = COPYING
+LIBRETRO_PICODRIVE_NON_COMMERCIAL = y
+LIBRETRO_PICODRIVE_GIT_SUBMODULES=y
+LIBRETRO_PICODRIVE_SITE_METHOD=git
+
+define LIBRETRO_PICODRIVE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile.libretro platform=aarch64
+endef
+
+define LIBRETRO_PICODRIVE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/picodrive_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/picodrive_libretro.so
+endef
+
+$(eval $(generic-package))

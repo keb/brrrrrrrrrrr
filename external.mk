e388bb40acdeb6464d0b41c4ee24236582ecbc4a
brrrrrrrrrr <brrrrrrrrrr@brrrrrrrrrr>
2024-01-27 01:23:58 -0500

    Add flto

--- external.mk
+++ external.mk
@@ -1,4 +1,9 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_BRRRRRRRRRR_PATH)/package/*/*.mk))
 
-TARGET_CFLAGS = -mtune=cortex-a55 -mcpu=cortex-a55+crc+crypto+fp+simd -Ofast -U_FORTIFY_SOURCE -fno-stack-protector -fno-stack-clash-protection -fomit-frame-pointer
+TARGET_CFLAGS = -mtune=cortex-a55 -mcpu=cortex-a55+crc+crypto+fp+simd -Ofast -U_FORTIFY_SOURCE -fno-stack-protector -fno-stack-clash-protection -fomit-frame-pointer -flto
+# -mfloat-abi=hard -mfpu=neon-fp-armv8
 TARGET_CXXFLAGS = $(TARGET_CFLAGS)
+
+# these break on lto
+NCURSES_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -fno-lto"
+READLINE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -fno-lto"

80ffac391fd415bb16010a78eb0a09f3601f8c48
wuaalb <20034857-wuaalb@users.noreply.gitlab.com>
2024-02-15 15:18:36 +0000

    add libretro-geolith (accurate neogeo)

---
+++ package/libretro-geolith/libretro-geolith.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# GEOLITH
+#
+################################################################################
+
+LIBRETRO_GEOLITH_VERSION = 38d3454ab4419f2be840d90400e66c596795d5c9
+LIBRETRO_GEOLITH_SITE = $(call github,libretro,geolith-libretro,$(LIBRETRO_GEOLITH_VERSION))
+LIBRETRO_GEOLITH_LICENSE = BSD-3-Clause
+LIBRETRO_GEOLITH_LICENSE_FILES = LICENSE
+
+define LIBRETRO_GEOLITH_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/libretro
+endef
+
+define LIBRETRO_GEOLITH_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/libretro/geolith_libretro.so $(TARGET_DIR)/usr/lib/libretro/geolith_libretro.so
+endef
+
+$(eval $(generic-package))

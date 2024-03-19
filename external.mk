fe73b3ec1f58ca3cc737e2531ae67c4ad087b7d1
brrrrrrrrrr <brrrrrrrrrr@brrrrrrrrrr>
2024-03-18 23:25:09 -0400

    Move patches out of external.mk to buildroot.patch

--- external.mk
+++ external.mk
@@ -2,9 +2,3 @@ include $(sort $(wildcard $(BR2_EXTERNAL_BRRRRRRRRRR_PATH)/package/*/*.mk))
 
 TARGET_CFLAGS = -mtune=cortex-a55 -mcpu=cortex-a55+crc+crypto+fp+simd -Ofast -U_FORTIFY_SOURCE -fno-stack-protector -fno-stack-clash-protection -fomit-frame-pointer -flto=auto
 TARGET_CXXFLAGS = $(TARGET_CFLAGS)
-
-# these break on lto
-NCURSES_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -fno-lto"
-READLINE_CONF_ENV = CFLAGS="$(TARGET_CFLAGS) -fno-lto"
-
-MESA3D_CONF_OPTS := $(subst -Dgallium-vc4-neon=disabled,,${MESA3D_CONF_OPTS})

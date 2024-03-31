50e29b59db25d5ca01a5961839e508a1a91a817f
brrrrrrrrrr <brrrrrrrrrr@brrrrrrrrrr>
2024-03-31 00:58:53 -0400

    Format acpi handler.sh

--- board/rgarc/rootfs_overlay/etc/acpi/handler.sh
+++ board/rgarc/rootfs_overlay/etc/acpi/handler.sh
@@ -4,35 +4,35 @@ joypad_dev=/dev/input/by-path/platform-gpio-keys-control-event-joystick
 mode_dev=/dev/input/by-path/platform-adc-keys-event-joystick
 
 case "$1" in
-    button/power)
-        /sbin/poweroff
-        ;;
-    button/volumeup)
-        if ! evtest --query $mode_dev EV_KEY BTN_MODE; then
-            brightnessctl s 1%+ -n 1 -q
-        elif ! evtest --query $joypad_dev EV_KEY BTN_SELECT; then
-        iwctl device wlan0 set-property Powered on
-        else
-            amixer sset Master 1%+ -q
-        fi
-        ;;
-    button/volumedown)
-        if ! evtest --query $mode_dev EV_KEY BTN_MODE; then
-            brightnessctl s 1%- -n 1 -q
-        elif ! evtest --query $joypad_dev EV_KEY BTN_SELECT; then
-        iwctl device wlan0 set-property Powered off
-        else
-            amixer sset Master 1%- -q
-        fi
-        ;;
-    jack/headphone)
-        case "$3" in
-            plug)
-                amixer -c 1 sset 'Internal Speakers' off
-            ;;
-            unplug)
-                amixer -c 1 sset 'Internal Speakers' on
-            ;;
-        esac
-    ;;
+button/power)
+	/sbin/poweroff
+	;;
+button/volumeup)
+	if ! evtest --query $mode_dev EV_KEY BTN_MODE; then
+		brightnessctl s 1%+ -n 1 -q
+	elif ! evtest --query $joypad_dev EV_KEY BTN_SELECT; then
+		iwctl device wlan0 set-property Powered on
+	else
+		amixer sset Master 1%+ -q
+	fi
+	;;
+button/volumedown)
+	if ! evtest --query $mode_dev EV_KEY BTN_MODE; then
+		brightnessctl s 1%- -n 1 -q
+	elif ! evtest --query $joypad_dev EV_KEY BTN_SELECT; then
+		iwctl device wlan0 set-property Powered off
+	else
+		amixer sset Master 1%- -q
+	fi
+	;;
+jack/headphone)
+	case "$3" in
+	plug)
+		amixer -c 1 sset 'Internal Speakers' off
+		;;
+	unplug)
+		amixer -c 1 sset 'Internal Speakers' on
+		;;
+	esac
+	;;
 esac

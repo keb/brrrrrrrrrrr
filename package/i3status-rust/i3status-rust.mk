68a8a11bb137b24acbb8552fdd4056c5b261d00e
brrrrrrrrrr <brrrrrrrrrr@brrrrrrrrrr>
2024-04-01 23:55:47 -0400

    Remove i3status and use i3status-rust
    - Far less resource usage
    - Looks nicer

--- package/i3status-rust/i3status-rust.mk
+++ package/i3status-rust/i3status-rust.mk
@@ -11,4 +11,14 @@ I3STATUS_RUST_LICENSE_FILES = LICENSE
 I3STATUS_RUST_DEPENDENCIES += alsa-utils upower lm-sensors
 I3STATUS_RUST_CARGO_BUILD_OPTS += --no-default-features
 
+define I3STATUS_RUST_INSTALL_TARGET_CMDS
+	# TODO fix path
+    $(INSTALL) -D -m 0755 -t $(TARGET_DIR)/usr/bin $(@D)/target/aarch64-unknown-linux-gnu/release/i3status-rs
+
+	# use $(INSTALL)
+	mkdir -p $(TARGET_DIR)/usr/share/i3status-rust
+	cp -rv $(@D)/files/icons $(TARGET_DIR)/usr/share/i3status-rust/
+	cp -rv $(@D)/files/themes $(TARGET_DIR)/usr/share/i3status-rust/
+endef
+
 $(eval $(cargo-package))

cb02391ebd25e94e57318d50f9e61bcaef5c778b
keb <me@keb.im>
2024-12-16 21:13:28 -0500

    update sharp shimmerless and move overlays

--- board/rootfs_overlay/root/.config/retroarch/shaders/shaders_glsl/shaders/sharp-shimmerless-subpixel-rgb.glsl
+++ board/rootfs_overlay/root/.config/retroarch/shaders/shaders_glsl/shaders/sharp-shimmerless-subpixel-rgb.glsl
@@ -1,12 +1,9 @@
 /*
- * sharp-shimmerless
+ * sharp-shimmerless-vrgb
  * Author: zadpos
  * License: Public domain
  * 
- * A retro gaming shader for sharpest pixels with no aliasing/shimmering.
- * Instead of pixels as point samples, this shader considers pixels as
- * ideal rectangles forming a grid, and interpolates pixel by calculating
- * the surface area an input pixel would occupy on an output pixel.
+ * Sharp-Shimmerless shader for v-RGB subpixels
  */
 
 #if defined(VERTEX)
@@ -30,9 +27,9 @@
 COMPAT_ATTRIBUTE vec4 VertexCoord;
 COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_ATTRIBUTE vec4 TexCoord;
-COMPAT_VARYING vec2 pixel;
-COMPAT_VARYING vec2 scale;
-COMPAT_VARYING vec2 invscale;
+COMPAT_VARYING vec4 pixel;
+COMPAT_VARYING vec4 scale;
+COMPAT_VARYING vec4 invscale;
 
 uniform mat4 MVPMatrix;
 uniform COMPAT_PRECISION int FrameDirection;
@@ -44,9 +41,14 @@ uniform COMPAT_PRECISION vec2 InputSize;
 void main()
 {
     gl_Position = MVPMatrix * VertexCoord;
-    pixel = TexCoord.xy * OutputSize * TextureSize / InputSize;
-    scale = OutputSize / InputSize;
-    invscale = InputSize / OutputSize;
+
+    vec2 pixel_xy = TexCoord.xy * OutputSize * TextureSize / InputSize;
+    vec2 scale_xy = OutputSize / InputSize;
+    vec2 invscale_xy = InputSize / OutputSize;
+    
+    pixel = pixel_xy.xxxy;
+    scale = scale_xy.xxxy;
+    invscale = invscale_xy.xxxy;
 }
 
 #elif defined(FRAGMENT)
@@ -78,23 +80,28 @@ uniform COMPAT_PRECISION vec2 OutputSize;
 uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 uniform sampler2D Texture;
-COMPAT_VARYING vec2 pixel; // texel to pixel scale
-COMPAT_VARYING vec2 scale; // pixel to texel scale
-COMPAT_VARYING vec2 invscale;
+COMPAT_VARYING vec4 pixel;
+COMPAT_VARYING vec4 scale;
+COMPAT_VARYING vec4 invscale;
 
 void main()
 {
-    // pixel: output screen pixels
-    // texel: input texels == game pixels
-    vec2 pixel_tl = floor(pixel);   // top-left of the pixel
-    vec2 pixel_br = ceil(pixel);    // bottom-right of the pixel
-    vec2 texel_tl = floor(invscale * pixel_tl); // texel the top-left of the pixel lies in
-    vec2 texel_br = floor(invscale * pixel_br); // texel the bottom-right of the pixel lies in
+    vec4 pixel_tl = floor(pixel);
+    pixel_tl.y -= 0.33;
+    pixel_tl.w += 0.33;
+    vec4 pixel_br = ceil(pixel);
+    pixel_br.y -= 0.33;
+    pixel_br.w += 0.33;
 
-    // the sampling point to get the correct box-filtered value
-    vec2 mod_texel = texel_br + vec2(0.5, 0.5);
-    mod_texel -= (vec2(1.0, 1.0) - step(texel_br, texel_tl)) * (scale * texel_br - pixel_tl);
-    
-    FragColor = vec4(COMPAT_TEXTURE(Texture, mod_texel / TextureSize).rgb, 1.0);
+    vec4 texel_tl = floor(invscale * pixel_tl);
+    vec4 texel_br = floor(invscale * pixel_br);
+
+    vec4 mod_texel = texel_br + vec4(0.5, 0.5, 0.5, 0.5);
+    mod_texel -= (vec4(1.0, 1.0, 1.0, 1.0) - step(texel_br, texel_tl)) * (scale * texel_br - pixel_tl);
+
+    FragColor.r = COMPAT_TEXTURE(Texture, mod_texel.xw / TextureSize).r;
+    FragColor.g = COMPAT_TEXTURE(Texture, mod_texel.yw / TextureSize).g;
+    FragColor.b = COMPAT_TEXTURE(Texture, mod_texel.zw / TextureSize).b;
+    FragColor.a = 1.0;
 } 
 #endif
